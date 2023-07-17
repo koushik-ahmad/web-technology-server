7021578e0d405f8a06bf565ac2bc06c8382a705d
koushik-ahmad <koushikahmad123@gmail.com>
2023-07-17 18:51:14 +0600

    load data

--- index.js
+++ index.js
@@ -5,9 +5,32 @@ const Port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+const details = require('./data/details.json');
 
 app.get('/', (req, res) => {
     res.send("Web technology API Running");
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const singleCourses = courses.find(c => c.id === id);
+    res.send(singleCourses);
+})
+
+
+app.get('/details', (req, res) => {
+    res.send(details);
+})
+
+app.get('/details/:id', (req, res) => {
+    const id = req.params.id;
+    const singleDetails = details.find(c => c.id === id);
+    res.send(singleDetails);
 })
 
 app.listen(Port, () => {
